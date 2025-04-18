1ff9c7939b72cb1cc3225f2e503b1a420270624c
minhkhoiv12 <khoitk14033@gmail.com>
2025-04-18 16:50:23 +0700

    Create Blog form And validate

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
+// User related routes
 Route::get('/', [UserController::class, "showCorrectHomepage"]);
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
+
+// Blog post related routes
+Route::get('/create-post', [PostController::class, 'showCreateForm']);
+Route::post('/create-post', [PostController::class, 'storeNewPost']);
